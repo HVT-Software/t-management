50604e5d3d70c7bb8d07799506f483cc8de6a825
Hồ Văn Toàn <it.vantoan@gmail.com>
2025-03-24 23:48:17 +0700

    Refactor: update pre-commit hooks and add linting script for improved code quality

--- lib/models/transaction.ts
+++ lib/models/transaction.ts
@@ -1,5 +1,5 @@
-import { IsUUID, IsNumber, IsString, IsDate, ValidateNested, IsEnum } from "class-validator";
 import { Type } from "class-transformer";
+import { IsDate, IsEnum, IsNumber, IsString, ValidateNested } from "class-validator";
 import { Category } from "./category";
 
 export enum TransactionType {
